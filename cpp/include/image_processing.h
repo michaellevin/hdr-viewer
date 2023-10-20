877637a8966673d72358c724b14b69f2f50a4a25
Michael Levin <michael.levi.fx@gmail.com>
2023-10-20 16:12:04 +0300

    fix: typo

--- cpp/include/image_processing.h
+++ cpp/include/image_processing.h
@@ -7,7 +7,7 @@
 #endif
 #define CL_HPP_ENABLE_EXCEPTIONS 1
 // #include <CL/opencl.hpp>
-if __has_include (<CL / opencl.hpp>)
+#if __has_include(<CL/opencl.hpp>)
 #include <CL/opencl.hpp>
 #else
 #include <opencl.h>
@@ -16,23 +16,23 @@ if __has_include (<CL / opencl.hpp>)
 #include <map>
 #include <string>
 
-    class ImageProcessor {
-       public:
-        ImageProcessor();
-        std::vector<float> apply_kernel(
-            std::vector<float>& pixels, const std::string& kernel_name,
-            const std::vector<float>& parameters) const;
-        // void apply_gamma_correction(std::vector<float>& pixels, float
-        // inv_gamma);
-        std::vector<float> apply_gamma_correction(std::vector<float>& pixels,
-                                                  float inv_gamma) const;
-        std::vector<float> apply_exposure_correction(std::vector<float>& pixels,
-                                                     float exposure) const;
-        std::vector<float> apply_exposure_gamma_correction(
-            std::vector<float>& pixels, float exposure, float inv_gamma) const;
+class ImageProcessor {
+   public:
+    ImageProcessor();
+    std::vector<float> apply_kernel(std::vector<float>& pixels,
+                                    const std::string& kernel_name,
+                                    const std::vector<float>& parameters) const;
+    // void apply_gamma_correction(std::vector<float>& pixels, float
+    // inv_gamma);
+    std::vector<float> apply_gamma_correction(std::vector<float>& pixels,
+                                              float inv_gamma) const;
+    std::vector<float> apply_exposure_correction(std::vector<float>& pixels,
+                                                 float exposure) const;
+    std::vector<float> apply_exposure_gamma_correction(
+        std::vector<float>& pixels, float exposure, float inv_gamma) const;
 
-       private:
-        cl::Context context;
-        // cl::Program program;
-        std::map<std::string, cl::Program> programs;
-    };
+   private:
+    cl::Context context;
+    // cl::Program program;
+    std::map<std::string, cl::Program> programs;
+};
